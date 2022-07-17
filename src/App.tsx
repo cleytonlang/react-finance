d6f853f776e524d91ee79fe949aaad81c8626328
Cleyton <cleytonb40@gmail.com>
2022-07-17 02:41:48 -0400

    add styled-componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import styled from "styled-components";
+const Title = styled.h1`
+  font-size: 2.5em;
+  color: #ff0000;
+`;
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
