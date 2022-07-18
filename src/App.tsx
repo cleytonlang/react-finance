19cd37e750578048d0db3f3292b26e0c77ff7498
Cleyton <cleytonb40@gmail.com>
2022-07-18 03:30:17 -0400

    create styled components global

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import styled from "styled-components";
-const Title = styled.h1`
-  font-size: 2.5em;
-  color: #ff0000;
-`;
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
